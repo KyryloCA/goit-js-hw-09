8e9ace25487fdf93ae5aa90bd413d58a0beb6310
KyryloCA <107942772+KyryloCA@users.noreply.github.com>
2023-02-07 16:08:07 +0000

    Deploying to gh-pages from @ KyryloCA/goit-js-hw-09@5608ae1ae3079d69e95366d37d0a8d12a97e6daa 🚀

--- 01-color-switcher.321bfef7.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let o=null;t.addEventListener("click",(function(){t.setAttribute("disabled",!0),o=setInterval((()=>r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled",!1),clearInterval(o)}));
-//# sourceMappingURL=01-color-switcher.321bfef7.js.map
--- 01-color-switcher.d40feb74.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),r=null;t.addEventListener("click",(function(){t.setAttribute("disabled",!0),r=setInterval((function(){return n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled",!1),clearInterval(r)}))}();
-//# sourceMappingURL=01-color-switcher.d40feb74.js.map
